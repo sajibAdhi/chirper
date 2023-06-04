1eaeb9b76b5b9d07afcdd4b29f91df55df9a7a58
Sajib Adhikary <tosajibadhi@gmail.com>
2023-06-04 14:33:50 +0600

    Notification & Event

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,16 +2,26 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property mixed $user
+ * @property mixed $message
+ * @property mixed $user_id
+ */
 class Chirp extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'message',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
     ];
 
     public function user(): BelongsTo
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,11 +2,11 @@
 
 namespace App\Providers;
 
+use App\Events\ChirpCreated;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
-
+use App\Listeners\SendChirpCreatedNotifications;
 class EventServiceProvider extends ServiceProvider
 {
     /**
@@ -15,6 +15,10 @@ class EventServiceProvider extends ServiceProvider
      * @var array<class-string, array<int, class-string>>
      */
     protected $listen = [
+        ChirpCreated::class => [
+            SendChirpCreatedNotifications::class,
+        ],
+
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
